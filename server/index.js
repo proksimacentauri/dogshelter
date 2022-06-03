42c91d9d9b3996a1f3eaf4c096a0cd561e475959
Donna <donnasoujeh@gmail.com>
2022-06-03 18:16:18 +0200

    add mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys =  require('./config/keys');
+
 const app = express();
+mongoose.connect(keys.mongoURI);
 
 app.get('/',(req, res) => {
   res.send({ test: 'test'});
